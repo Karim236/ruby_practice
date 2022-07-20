6e1bb8f7e09d7a1271ddf109b5f8d3bdb51e0181
unknown <karimboy2306@gmail.com>
2022-07-20 19:48:41 +0300

    add new car for train

--- example.rb
+++ example.rb
@@ -8,6 +8,7 @@ include Truck
 
 bmw = Car::Car.new("BMW", "X5", "Black", 100, true)
 kia = Car::Car.new("Kia", "K5", "White", 200, true)
+audi = Car::Car.new("Audi", "Q7", "Black", 350, false)
 
 ducati = Moto::Moto.new("Ducati", "M1", "Blue", 200, true)
 yamaha = Moto::Moto.new("Yamaha", "Y5", "Yellow", 100, true)
